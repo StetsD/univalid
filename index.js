8e8e6bedf14b0ca4e2fab401b5388d72a53b0cc3
StetsD <chortstd@gmail.com>
2018-03-01 09:02:34 +0300

    init

---
+++ index.js
@@ -0,0 +1,175 @@
+'use strict';
+
+const {EventEmitter} = require('events');
+const UnivalidStrategyDefault = require('univalid-strategy-default');
+
+const { notEmpty,
+        isObject,
+        notZeroLength,
+        isArray } = require('./lib/univalid-error-handler');
+const decision = require('./lib/univalid-decision');
+const MSG_CONFIG = require('./lib/univalid-msg-config')();
+
+
+module.exports = () => {
+	let _strategy = null;
+	var _state = [];
+	let _validationHandlers = {};
+	let _msgConfig = MSG_CONFIG;
+
+	class Univalid extends EventEmitter {
+	    constructor(){
+	        super();
+
+	        this.on('error', msg => {
+	            console.warn(new Error(msg));
+	        });
+
+			this.setStrategy(new UnivalidStrategyDefault());
+			return this;
+	    }
+
+	    check(pack){
+	        isArray(pack, 'Entry validation package must be an array type');
+
+	        this.emit('start:valid', this);
+			this.clearState();
+	        pack.length && _strategy.check(pack, this);
+	        this.emit('end:valid', this);
+	        return this;
+	    }
+
+	    validate(field){
+	        let {name, val, type, filter, msg} = field;
+	        let handler = _validationHandlers[type];
+
+			this.emit('start:valid:field', field);
+
+			if(!name)
+				return this.emit('error', 'Can\'t find "name" field, "name" is required field')
+			if(!type)
+				return this.emit('error', `Can't find "type" field in ${name}, "type" is required field`);
+
+	        if(handler){
+	            let condition = handler(val);
+	            var {state, status} = decision(val, condition);
+	            var msgResult;
+
+				if(filter && !_strategy.applyFilter(filter, val)){
+					state = 'error';
+					status = 'filter';
+				}
+
+				msg && msg[status] ?
+					msgResult = msg[status] :
+					msgResult = _msgConfig[status];
+
+	            _state.push({name, type, state, status, msg: msgResult});
+	        }
+
+			this.emit('end:valid:field', {name, type, state, status, msg: msgResult});
+	    }
+
+		setStrategy(strategy){
+	        notEmpty(strategy, 'Strategy of validation is not defined');
+	        isObject(strategy, 'Strategy must be an object');
+
+	        _strategy = strategy;
+			this.setValidHandler(strategy.getValidationHandlers());
+			this.emit('change:strategy', this);
+	        return this;
+	    }
+
+	    setValidHandler(pack){
+	        notEmpty(pack, 'Pack of validation handlers is not defined');
+	        isObject(pack, 'Pack of validation handlers must be an object type');
+
+	        if(Object.keys(pack).length !== 0){
+	            for(let key in pack){
+	                _validationHandlers[key] = pack[key];
+	                this.emit('set:new-ValidationHandler', key, pack[key]);
+	            }
+	        }else{
+	            this.emit('error', 'Pack of validation is empty');
+	        }
+	    }
+
+		setMsgConfig(config){
+			if(!config){
+				return this.emit('error', 'msgConfig of validation handlers is not defined');
+			}
+			let notAllReqFields = true;
+			['empty', 'invalid', 'filter', 'success'].forEach(field => {
+				if(!config[field]){
+					notAllReqFields = false;
+					return this.emit('error', `The "${field}" field is required in msgConfig`);
+				}
+			});
+
+			if(notAllReqFields){
+				_msgConfig = config;
+				this.emit('change:msg-config', this);
+			}
+		}
+
+		set(option, val){
+			let strOpt = _strategy[option];
+
+			if(!strOpt)
+				return this.emit('error', `The option "${option}" is not defined in used strategy`);
+			if(val){
+				let valType = Array.isArray(val) ? 'array' : typeof val,
+					strOptType = Array.isArray(strOpt) ? 'array' : typeof strOpt;
+
+				if(valType !== strOptType)
+					return this.emit('error', `The value "${val}" of setter has bad type`);
+
+				_strategy.set(option, val);
+				this.emit('set:new-prop-strategy', this);
+			}
+		}
+
+		get(prop, ...args){
+			if(!_strategy[prop])
+				return this.emit('error', `The property "${prop}" is not defined in used strategy`);
+
+			if(typeof _strategy[prop] === 'function'){
+				return _strategy.get(prop).apply(_strategy, args);
+			}
+
+			return _strategy.get(prop);
+		}
+
+		clearState(){
+			_state = [];
+			this.emit('clear:state', this);
+		}
+
+		get getValidHandler(){
+	        return _validationHandlers;
+	    }
+
+	    get getState(){
+	        return _state;
+	    }
+
+		get getStrategy(){
+			return _strategy;
+		}
+
+		get getCommonState(){
+			if(_state.length){
+				for(let i = 0; i < _state.length; i++){
+					if(_state[i].state === 'error')
+						return 'error';
+				}
+
+				return 'success';
+			}else{
+				return null;
+			}
+		}
+	}
+
+    return new Univalid();
+};
---
+++ test/test-univalid.js
@@ -0,0 +1,77 @@
+var assert = require('assert');
+let univalid = require('../index')();
+
+describe('Univalid', function() {
+
+    describe('Univalid API', function() {
+
+		it('Must have default univalid strategy of validation', function() {
+			assert.ok(univalid.getStrategy);
+		});
+
+		it('Univalid has set method', function() {
+        	assert.ifError(univalid.set('passConfig', {min: 6, analysis: ['hasUppercase']}));
+        });
+
+		it('setValidHandler method works ok', function() {
+        	assert.ifError(univalid.setValidHandler({
+				'superRequred': (val) => {
+					return true;
+			    },
+			}));
+        });
+
+
+		it('validate method works ok', function() {
+        	assert.ifError(univalid.validate({
+				name: 'username',
+				val: 'one',
+				type: 'required',
+				filter: 'oL'
+			}));
+        });
+
+		it('check method works ok', function() {
+			let univalid = require('../index')();
+
+        	assert(univalid.check([
+				{
+		            name: 'email',
+		            val: 'Uriy@mazafaka.com',
+		            type: 'email',
+		            filter: 'oL',
+		            msg: {
+		                empty: 'You shall not pass',
+		                invalid: 'Bad email',
+		                filter: 'Only lat/numbers/specials symbols',
+		                success: 'All right'
+		            }
+		        }
+			]));
+        });
+    });
+
+	describe('Univalid Check Tests', function(){
+		it('Validate method returned object with correct fields', function(){
+			let univalid = require('../index')();
+
+			univalid.validate({
+				name: 'username',
+				val: 'one',
+				type: 'required',
+				filter: 'oL'
+			});
+
+			let result = univalid.getState[0];
+			let {name, type, state, status, msg} = result;
+
+			assert(typeof result === 'object');
+			assert(result.name && typeof result.name === 'string');
+			assert(result.type && typeof result.type === 'string');
+			assert(result.state && typeof result.state === 'string');
+			assert(result.status && typeof result.status === 'string');
+			assert(result.msg && typeof result.msg === 'string');
+		});
+	});
+
+});
